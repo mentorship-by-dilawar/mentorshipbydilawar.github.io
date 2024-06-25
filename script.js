b74475ac0342cdbf80a64363724eaec1c83e15f9
mentorship-by-dilawar <142672964+mentorship-by-dilawar@users.noreply.github.com>
2024-06-25 10:25:13 +0500

    ss

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function showNotification(programType) {
 
     const toast = Toastify({
         text: message,
-        duration: 10000, // Duration in milliseconds (5 seconds)
+        duration: 5000, // Duration in milliseconds (5 seconds)
         close: true,
         gravity: "bottom", // Display position: "top" or "bottom"
         position: "center", // Alignment: "left", "center", or "right"
@@ -49,5 +49,5 @@ setInterval(() => {
     showNotification("1to1");
     setTimeout(() => {
         showNotification("2months");
-    }, 10000);
-}, 15000); // 10 seconds interval for alternating notifications
+    }, 5000);
+}, 20000); // 10 seconds interval for alternating notifications
